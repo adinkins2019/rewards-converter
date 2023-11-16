91c59ef682ae914ff8090012e4c42932cfac7e69
Antonio Dinkins <antonio.dinkins2019@gmail.com>
2023-11-16 17:56:23 -0500

    "feat: Added RewardValue class to project"

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,17 @@
+import java.math.BigDecimal;
+
+public class RewardValue {
+    public Double getMilesValue() {
+        return cashValue.doubleValue() /0.0035;
+    }
+
+    public double getCashValue() {
+        return miles*0.0035;
+    }
+
+    Long miles;
+    Double cashValue;
+    public RewardValue(Double cashValue){ this.cashValue = cashValue;}
+    public RewardValue(Long miles){this.miles = miles;}
+
+}
